0bf1a062c46d7249582afd2c69ba68306c57ffb1
madi <madi@madanie.com>
2024-02-20 18:39:48 +0200

    added ex

--- mutil.go
+++ mutil.go
@@ -1,6 +1,7 @@
 package m;
 import (
    "fmt"
+   "os"
 )
 func Er(msg string, err error) bool{
    if err != nil { 
@@ -8,4 +9,16 @@ func Er(msg string, err error) bool{
       return true;
    }
    return false;
+}
+func Ex(msg string, err error){
+   if err != nil { 
+      fmt.Print( fmt.Errorf("ERROR: %s: %v", msg, err));
+      os.Exit(3)
+   }
+}
+func Ep(msg string, err error){
+   if err != nil { 
+      fmt.Print( fmt.Errorf("ERROR: %s: %v", msg, err));
+      panic(err);
+   }
 }
